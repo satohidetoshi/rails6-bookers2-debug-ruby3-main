960440b556615e4fe72936f5dda0b8cd6447b03b
EC2 Default User <ec2-user@ip-172-31-10-174.ap-northeast-1.compute.internal>
2023-04-10 02:41:58 +0000

    search2

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,27 +5,31 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :books
-  
+
+
+  has_many :book_comments, dependent: :destroy
+  has_many :favorites, dependent: :destroy
+
   # 自分がフォローされる（被フォロー）側の関係性
   has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
   # 被フォロー関係を通じて参照→自分をフォローしている人
   has_many :followers, through: :reverse_of_relationships, source: :follower
-  
+
   # 自分がフォローする（与フォロー）側の関係性
   has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
   # 与フォロー関係を通じて参照→自分がフォローしている人
   has_many :followings, through: :relationships, source: :followed
-  
+
   has_one_attached :profile_image
 
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
   validates :introduction, length: { maximum: 50 }
-  
-  
+
+
   def get_profile_image
     (profile_image.attached?) ? profile_image : 'no_image.jpg'
   end
-  
+
   def follow(user)
     relationships.create(followed_id: user.id)
   end
@@ -37,8 +41,8 @@ class User < ApplicationRecord
   def following?(user)
     followings.include?(user)
   end
-  
-  
+
+
   def self.search_for(content, method)
     if method == 'perfect'
       User.where(name: content)
@@ -50,5 +54,5 @@ class User < ApplicationRecord
       User.where('name LIKE ?', '%' + content + '%')
     end
   end
-  
+
 end
